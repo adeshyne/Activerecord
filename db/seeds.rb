386892dc399a3f88f4fca1510eedcd9dfbb4462f
Ade Akintide <ade@abreezy.local>
2015-12-10 12:50:58 -0500

    second commit

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+require './models.rb'
+User.create({
+	:name => "Jeremy",
+	:email => "jeremy.andaya@gmail.com"
+	})
+User.create({
+	:name => "John",
+	:email => "john@gmail.com"
+	})
+User.create({
+	:name => "Tony",
+	:email => "tony@gmail.com"
+	})
+Status API Training Shop Blog About Pricing
